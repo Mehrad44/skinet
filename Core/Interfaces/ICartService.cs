59ceba5c714b3a9e253be55a8d4c21ea035539c3
Mehrad44 <mehrad.langrud@outlook.com>
2026-02-12 20:04:00 +0330

    Add Redis Database

---
+++ Core/Interfaces/ICartService.cs
@@ -0,0 +1,18 @@
+using System;
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface ICardService
+{
+    Task<ShoppingCart?> GetCartAsync(string key);
+
+    Task<ShoppingCart?> SetCartAsync(ShoppingCart cart);
+
+    Task<bool> DeletCartAsync(string key);
+
+    
+
+
+
+}
