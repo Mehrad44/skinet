d4b36b948a99ade94ee6ea5c44a60a9f66c19393
Mehrad44 <mehrad.langrud@outlook.com>
2026-02-19 00:24:37 +0330

    Add Identity to API

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    public string firstName {get; set;} = string.Empty;
+
+    [Required]
+    public string LastName {get; set;} = string.Empty;
+
+
+    [Required]
+    public string Email {get; set;} = string.Empty;
+
+
+    [Required]
+    public string Password {get; set;} = string.Empty;
+
+
+
+}
